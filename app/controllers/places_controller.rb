c9cdc14225070d065dfcef7548a08ed624fec148
Rebecca Taylor <inflamedmuse@gmail.com>
2015-10-29 05:56:13 +0000

    Adjusted code so that only the user who created a place can edit and update it

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 	def index
 		@places = Place.all.order("created_at ASC").page(params[:page]).per(5)
@@ -20,10 +20,18 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Sorry, not allowed in here!', :status => :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Dude, this isn\'t the place for you!', :status => :forbidden
+		end
+		
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
